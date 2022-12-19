d2a6bfaa0801d18b8cf67e80e2dc4b0435eda743
Arka Bhuiyan <arka.bhuiyan@bjitgroup.com>
2022-12-19 17:12:27 +0600

    Added more practice jquery code.

--- js/script.js
+++ js/script.js
@@ -72,8 +72,8 @@ $(function() {
   // $('#list').parent().css('background-color', 'rgba(180,180,30,0.8)');
   // $('#list').siblings(':header').css('background-color', 'rgba(180,180,30)');
   // $('#list').prev().css('background-color', 'rgba(180,180,30)'); //
-  $(':header').next().css("background-color", "rgba(180,180,30,0.8)"); //
-  $('form').children('input:text').css('background-color', 'rgba(180,180,30,0.8');
+  // $(':header').next().css("background-color", "rgba(180,180,30,0.8)"); //
+  // $('form').children('input:text').css('background-color', 'rgba(180,180,30,0.8');
 
   // Methods for filtering input
   // find all the even list items
@@ -91,11 +91,69 @@ $(function() {
   // Coloring the list items except the first one
   $('li').not(":first").css('background-color', 'rgba(180,180,30,0.6');
 });
+
+// Adding new items to the DOM using JQuery
 $(function () {
   $('ul ul:first').append('<li>I am the sub item.</li>');
   $('<li>I am the last item</li>').appendTo($('ul ul:first'));
   $('ul ul').prepend('<li class="firstItem">I am gonna be the first item</li>');
-  $('<li class="secondItem">I am gonna be the second item</li>').appendTo($('ul li ul.subList li.firstItem'));
-  $('p').after($('#list'));
+  $('<li class="secondItem">I am gonna be the second item</li>').appendTo($('ul li ul li.firstItem'));
+  // $('p').after($('#list'));
+  $('<h2>Hi. I am Arka Bhuiyan.</h2>').prependTo('#content');
   
-})
+});
+
+$(function () {
+  // $('li').replaceWith($('<li>I am the replaced item</li>'));
+  var firstList = $('li:first');
+  // $('p').replaceWith(firstList);
+  // replacing the red box and the green box with the blue box
+  var blueBox = $('<div class="blue-box">More Blue Around here</div>');
+  // $('.red-box').replaceWith(blueBox);
+  // $('.green-box').replaceWith(blueBox);
+  // bluebox replacing all the red and green boxes
+  // $(blueBox).replaceAll(".red-box, .green-box");
+});
+
+// removing elements using jQuery
+$(function () {
+  // $('li').remove();
+  // $("form").children().not("input[type='email']").remove();
+
+  // detaching the list item and attaching it again to the DOM
+  // let detachedItem = $("li").detach();
+  // $("#content").append(detachedItem);
+
+  // empty everything inside the paragraph
+  // $("p:first").empty();
+
+  // Empty everything inside the boxes, without removing them. 
+  $(".red-box, .green-box, .blue-box").empty();
+});
+$(function () {
+  let firstList = $("ul#list li ul li");
+  $.ajax({
+    type: 'GET',
+    dataType: 'JSON',
+    url: "https://reqres.in/api/users",
+    success: function (response) {
+      if (response != null) {
+        console.log(response.data);
+        // console.log(response.data[0]);
+        for (let i in response.data) {
+            let email = response.data[i].email;
+            console.log(response.data[i]);
+            console.log(email);
+            // var listElement = $("<li>",{"email:":response.data[i].email},"</li>");
+            // $("ul li ul li").append($("li").text(response.data[i].email));
+          // $("li").text(email).appendTo($("ul li ul li"));
+          let temp = `<li>email:${email}</li>`;
+          $("ul ul").append(temp);
+            }
+        }
+    },
+    error: function () {
+        console.log("error in the api.");
+    }
+});
+});  
